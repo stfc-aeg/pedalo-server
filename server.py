12541a5323cf42f6a919bd43eea97d0e92a9b3ee
Vladimir Garanin <vladimir.garanin@stfc.ac.uk>
2021-01-21 13:51:35 +0000

    Add a way of recording data to csv

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
+from tornado import httputil
 import tornado.httpserver
 import tornado.websocket
 import tornado.ioloop
 import tornado.web
 import time
+import csv
 from tornado.concurrent import run_on_executor
 from concurrent import futures
 
@@ -12,6 +14,7 @@ class WSHandler(tornado.websocket.WebSocketHandler):
 
     def open(self):
         print("New connection")
+        self.f = open("test.csv", "a", newline='')
         self.sensor_data_pull_method()
 
     def on_message(self, message):
@@ -20,6 +23,7 @@ class WSHandler(tornado.websocket.WebSocketHandler):
 
     def on_close(self):
         print("Connection closed")
+        self.f.close()
 
     def check_origin(self, origin):
         return True
@@ -27,9 +31,13 @@ class WSHandler(tornado.websocket.WebSocketHandler):
     #TODO parametarise sleep time and While True
     @run_on_executor
     def sensor_data_pull_method(self):
+        fieldnames = ["TestStr", "TestNum"]
+        writter = csv.DictWriter(self.f, fieldnames=fieldnames)
+        writter.writeheader()
         while True:
+            writter.writerow({"TestStr" : "Test", "TestNum" : "123"})
+            self.f.flush()
             time.sleep(2)
-            print("I am pulling")
 
 application = tornado.web.Application([(r'/', WSHandler)])
 
