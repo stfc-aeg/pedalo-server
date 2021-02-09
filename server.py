43a720cfd93ab6264cee2d832d170440018e088b
Vladimir Garanin <vladimir.garanin@stfc.ac.uk>
2021-02-09 14:33:25 +0000

    Encapsulate code

--- server.py
+++ server.py
@@ -10,31 +10,55 @@ import json
 from tornado.concurrent import run_on_executor
 from concurrent import futures
 
-if (len(sys.argv) == 2):
-    import bme680
-    try:
-        sensor = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
-    except IOError:
-        sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
-    sensor.set_temperature_oversample(bme680.OS_8X)
-    sensor.set_filter(bme680.FILTER_SIZE_3)
+class Server(tornado.web.Application):
 
-def sensor_data_pull():
-    try:
-        if sensor.get_sensor_data():
-            dataFromSensorFunc = sensor.data.temperature
-    except:
-            dataFromSensorFunc = str(random.randint(0,10))
-    return dataFromSensorFunc
+    def __init__(self):
+        self.executor = futures.ThreadPoolExecutor(max_workers=1)
+        self.sensor_loaded = False
+        self.load_sensors()
+        self.temperature = 0
+        handlers = [(r'/', WSHandler, dict(server = self))]
+        super().__init__(handlers)
+        self.sensor_data_pull_method()
+
+    def load_sensors(self):
+        try:
+            import bme680
+            sensor = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
+            sensor.set_temperature_oversample(bme680.OS_8X)
+            sensor.set_filter(bme680.FILTER_SIZE_3)
+        except:
+            pass
+        print("Sensors are loaded")
+
+    def sensor_data_pull(self):
+        if self.sensor_loaded:
+            self.temperature = sensor.data.temperature
+        else:
+            self.temperature = str(random.randint(0,10))
+
+    def getTemperature(self):
+        return self.temperature
+
+    #TODO parametarise sleep time and While True
+    @run_on_executor
+    def sensor_data_pull_method(self):
+        # fieldnames = ["TestStr", "TestNum"]
+        # writter = csv.DictWriter(self.f, fieldnames=fieldnames)
+        # writter.writeheader()
+        while True:
+            self.sensor_data_pull()
+            # writter.writerow({"TestStr" : "Test", "TestNum" : "123"})
+            # self.f.flush()
+            time.sleep(0.5)
 
 class WSHandler(tornado.websocket.WebSocketHandler):
 
-    executor = futures.ThreadPoolExecutor(max_workers=1)
+    def initialize(self, server):
+        self.server = server #type: Server
 
     def open(self):
         print("New connection")
-        self.f = open("test.csv", "a", newline='')
-        self.sensor_data_pull_method()
 
     def on_message(self, message):
         print("Message received")
@@ -42,7 +66,6 @@ class WSHandler(tornado.websocket.WebSocketHandler):
 
     def on_close(self):
         print("Connection closed")
-        self.f.close()
 
     def check_origin(self, origin):
         return True
@@ -51,7 +74,8 @@ class WSHandler(tornado.websocket.WebSocketHandler):
         messageFromServerJson = json.loads(message)
         switch = {
             "Test Message" : self.TestMessage,
-            "Set offset" : self.setOffsetTemp
+            "Set offset" : self.setOffsetTemp,
+            "Get temperature" : self.getTemperature
         }
         func = switch.get(messageFromServerJson["Command"], lambda: "Invalid message")
         func(messageFromServerJson["Args"])
@@ -63,24 +87,14 @@ class WSHandler(tornado.websocket.WebSocketHandler):
         print(args[0])
         self.write_message("Finished")
 
+    def getTemperature(self, *args):
+        self.write_message(self.server.getTemperature())
 
-    #TODO parametarise sleep time and While True
-    @run_on_executor
-    def sensor_data_pull_method(self):
-        fieldnames = ["TestStr", "TestNum"]
-        writter = csv.DictWriter(self.f, fieldnames=fieldnames)
-        writter.writeheader()
-        while True:
-            global dataFromSensor
-            dataFromSensor = sensor_data_pull()
-            writter.writerow({"TestStr" : "Test", "TestNum" : "123"})
-            self.f.flush()
-            time.sleep(0.5)
-
-application = tornado.web.Application([(r'/', WSHandler)])
-
-if __name__ == "__main__":
-    http_server = tornado.httpserver.HTTPServer(application)
-    http_server.listen(8888)
+def main():
+    application = Server()
+    application.listen(8888)
     print("*** Websocket Server Started")
     tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+    main()
