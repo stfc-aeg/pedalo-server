43bdfd820b388ab3e0a0d33efe3d592e1c142b90
Vladimir Garanin <vladimir.garanin@stfc.ac.uk>
2021-02-23 11:24:32 +0000

    Follow PEP8 with bme280 file

--- bme280sensor.py
+++ bme280sensor.py
@@ -7,15 +7,16 @@ class bme280sensor(sensor):
     def __init__(self) -> None:
         super().__init__()
         try:
-            self.me = bme280.BME280(i2c_dev = SMBus(1))
+            self.me = bme280.BME280(i2c_dev=SMBus(1))
             self.me.update_sensor()
-        except:
+        except RuntimeError:
             raise bme280_not_found
 
     def pull_data(self):
         self.data["Temperature"] = self.me.get_temperature()
         self.data["Humidity"] = self.me.get_humidity()
         self.data["Pressure"] = self.me.get_pressure()
+
 
 class bme280_not_found(Exception):
     pass
