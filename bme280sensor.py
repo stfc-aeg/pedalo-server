5b07b1b66e2a276e03ec7a06033d0af56ded3511
Vladimir Garanin <vladimir.garanin@stfc.ac.uk>
2021-02-23 09:45:43 +0000

    Remove unwanted methods, remove iterfaces unwatned method

--- bme280sensor.py
+++ bme280sensor.py
@@ -4,8 +4,8 @@ from smbus import SMBus
 
 
 class bme280sensor(sensor):
-    def __init__(self,type) -> None:
-        super().__init__(type)
+    def __init__(self) -> None:
+        super().__init__()
         try:
             self.me = bme280.BME280(i2c_dev = SMBus(1))
             self.me.update_sensor()
@@ -16,11 +16,6 @@ class bme280sensor(sensor):
         self.data["Temperature"] = self.me.get_temperature()
         self.data["Humidity"] = self.me.get_humidity()
         self.data["Pressure"] = self.me.get_pressure()
-
-
-    def get_temperature(self):
-        self.pull_data()
-        return self.data["Temperature"]
 
 class bme280_not_found(Exception):
     pass
--- bme680sensor.py
+++ bme680sensor.py
@@ -2,15 +2,20 @@ import bme680
 from sensorInterface import sensor
 from smbus import SMBus
 
-class bme680sensor(sensor):
-    def __init__(self,type) -> None:
-        super().__init__(type)
+class bme680sensor(Sensor):
+    def __init__(self) -> None:
+        super().__init__()
         try:
             self.me = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
         except RuntimeError:
             raise bme680_not_found
         except OSError:
             raise bme680_not_found
+        self.data = {
+            "Temperature" : 0,
+            "Humidity" : 0,
+            "Pressure" : 1000
+        }
 
     def pull_data(self):
         if (self.me.get_sensor_data()):
@@ -19,11 +24,6 @@ class bme680sensor(sensor):
             self.data["Pressure"] = self.me.data.pressure
         else:
             pass
-
-
-    def get_temperature(self):
-        self.pull_data()
-        return self.data["Temperature"]
 
 class bme680_not_found(Exception):
     pass
